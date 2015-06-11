39d0e429787362b4904c8a7babde719eb9f3aafb
Adam Farden <adam@farden.cz>
2015-06-11 23:58:49 +0200

    Revert "seandroid: Adding policy from other directories"
    AOSP does not like recursive references
    
    This reverts commit 5d0f9589543c693cbdc2f3b07736f7d67c129998.
    
    Change-Id: Id612f9b1b590de4d38581072a72c196c106cbb7b
    
    Conflicts:
    	sepolicy.mk

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,6 +1,5 @@
 # Board specific SELinux policy variable definitions
 BOARD_SEPOLICY_DIRS := \
-       $(BOARD_SEPOLICY_DIRS) \
        device/qcom/sepolicy \
        device/qcom/sepolicy/common \
        device/qcom/sepolicy/test \
