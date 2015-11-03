a8cbcaae8bb684b51cbe1d288fee4a11932ad448
David Viteri <davidteri91@gmail.com>
2015-11-03 10:50:33 +0100

    Revert "BOARD_SEPOLICY_UNION was dropped [2]"
    This reverts commit 2663dcb6584405ffbd58bcf695f8e75746b8f201.

--- Android.mk
+++ Android.mk
@@ -6,5 +6,5 @@
 # include device/sony/sepolicy/sepolicy.mk
 #
 # to your device's BoardConfig. It is highly recommended that in case
-# you have your own BOARD_SEPOLICY_DIRS declaration,
+# you have your own BOARD_SEPOLICY_DIRS and BOARD_SEPOLICY_UNION declarations,
 # the inclusion happens _before_ those lines
